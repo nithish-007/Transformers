106aa6ff697ca347b601b93f239efe0b991b4660
nithish-007 <itsmedecoder07@gmail.com>
2025-05-07 23:59:05 +0530

    multihead partiall added

--- attention_is_all_you_need/model.py
+++ attention_is_all_you_need/model.py
@@ -67,3 +67,13 @@ class MultiHeadAttentionBlock(nn.Module):
         assert d_model % h ==0, "d_model is not divisible by h"
     
         self.d_k = d_model // h
+        self.w_q = nn.linear(d_model, d_model) #Wq
+        self.w_k = nn.linear(d_model, d_model) #Wk
+        self.w_v =nn.Linear(d_model, d_model)  #Wv
+
+        self.w_o = nn.iinear(d_model, d_model) # Wo
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q)
+        key = 
